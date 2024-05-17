353226b64dbf7a9761c3fa3e1df3cc979c9e9df2
Daniel Groh <daniagroh@gmail.com>
2024-05-17 10:35:50 -0300

    fixing home page style

--- src/pages/AboutMe/AboutMe.tsx
+++ src/pages/AboutMe/AboutMe.tsx
@@ -1,37 +1,40 @@
 import developer from '../../assets/developer.png';
+import boy from '../../assets/boy.png';
 import * as S from './styles';
 
 function AboutMe() {
   return (
     <S.Container>
       <S.Intro>
-        <S.Greeting>
-          <span>
-            Hello, 👋🏻
-            <br />
-            I&apos;m Daniel Arruda Groh
-            <br />A software engineer
-          </span>
-        </S.Greeting>
-        <S.ProfileImage>
-          <img src={developer} alt="ProfilePhoto" />
-        </S.ProfileImage>
+        <span>
+          Hello, 👋🏻
+          <br />
+          I&apos;m Daniel Arruda Groh
+          <br />
+          Software engineer
+        </span>
+        <img src={developer} alt="Developer" />
       </S.Intro>
       <S.Info>
-        I have a degree in computer engineering and during my degree I have
-        worked with different tools. I&apos;m currently a front-end developer at{' '}
-        <a href="https://www.receba.app" target="_blank" rel="noreferrer">
-          Receba
-        </a>
-        , but have already worked as back-end developer at{' '}
-        <a href="https://www.iclubs.tech" target="_blank" rel="noreferrer">
-          Iclubs
-        </a>{' '}
-        and as software tester at{' '}
-        <a href="https://www.zappts.com.br" target="_blank" rel="noreferrer">
-          Zappts
-        </a>
-        .
+        <span>
+          I have a degree in computer engineering and during my degree I have
+          worked with different tools. I&apos;m currently a front-end developer
+          at{' '}
+          <a href="https://www.receba.app" target="_blank" rel="noreferrer">
+            Receba
+          </a>
+          , but have already worked as back-end developer at{' '}
+          <a href="https://www.iclubs.tech" target="_blank" rel="noreferrer">
+            Iclubs
+          </a>{' '}
+          and as software tester at{' '}
+          <a href="https://www.zappts.com.br" target="_blank" rel="noreferrer">
+            Zappts
+          </a>
+          .
+        </span>
+
+        <img src={boy} alt="Boy" />
       </S.Info>
     </S.Container>
   );
--- src/pages/AboutMe/styles.ts
+++ src/pages/AboutMe/styles.ts
@@ -5,40 +5,33 @@ export const Container = styled.div``;
 
 export const Intro = styled.div`
   display: flex;
-  padding-top: 10px;
-  padding-bottom: 10px;
   align-items: center;
+  height: 100vh;
   justify-content: space-between;
-`;
-
-export const Greeting = styled.div`
-  display: block;
-  justify-content: center;
-  align-items: center;
-  span {
-    font-size: 32px;
-  }
-`;
-
-export const ProfileImage = styled.div`
-  display: flex;
-  justify-content: flex-end;
-
+  font-size: 45px;
   img {
-    width: 150px;
-    max-width: 100%;
-    height: auto;
-    border-radius: 150px;
+    display: flex;
+    align-items: end;
+    width: 40%;
   }
 `;
 
 export const Info = styled.div`
-  padding-top: 10px;
-  padding-bottom: 10px;
-  font-size: 28px;
+  display: flex;
+  align-items: center;
+  height: 80vh;
   text-align: center;
+  font-size: 30px;
+
   a {
     color: ${theme.colors.black};
     font-weight: 700;
+  }
+
+  img {
+    padding: 0px 20px;
+    display: flex;
+    align-items: end;
+    width: 30%;
   }
 `;
