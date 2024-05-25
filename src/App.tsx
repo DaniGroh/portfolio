89ff0f56e3d11a560b18762ba794d0bcf606f806
Daniel Groh <daniagroh@gmail.com>
2024-05-24 21:17:57 -0300

    adding vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 import Routes from './routes';
 import GlobalStyle from './styles/global';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <SpeedInsights />
+      <Analytics />
       <Routes />
       <GlobalStyle />
     </>
