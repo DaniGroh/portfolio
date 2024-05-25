51c530fd26a7d1be523a2cb6cc939e775d0e6a4c
Daniel Groh <daniagroh@gmail.com>
2024-05-24 21:01:47 -0300

    adding vercel speedinsight

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import Routes from './routes';
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <>
+      <SpeedInsights />
       <Routes />
       <GlobalStyle />
     </>
